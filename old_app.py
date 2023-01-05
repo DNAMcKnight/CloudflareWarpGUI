14d7e19c6c78d59f576b05633a2d39bc9308c7c2
DNAMcKnight <conner.cmk9040@gmail.com>
2023-01-05 23:29:56 +0600

    added proper checks for requirements to run the script

--- old_app.py
+++ old_app.py
@@ -1,12 +1,14 @@
-from tkinter import Tk, Label, Button
+from tkinter import Tk, Label, Button,messagebox
 from tkinter import StringVar, PhotoImage
 from threading import Thread
 from os import popen, getcwd
 from time import sleep
+import sys
 
 root = Tk()
 root.title("Cloudflare WARP")
 root.geometry("470x200")
+root.resizable(False, False)
 white = PhotoImage(file=f"{getcwd()}/images/white.png")
 orange = PhotoImage(file=f"{getcwd()}/images/orange.png")
 root.iconphoto(True, white)
@@ -15,7 +17,17 @@ enableLabel = Label(root, text="Enable Cloudflare WARP", width=50).grid(row=1, c
 disableLabel = Label(root, text="Disable Cloudflare WARP", width=50).grid(row=2, column=1)
 statusLabel = Label(root, textvariable=status, width=50).grid(row=3, column=1, columnspan=2, pady=110)
 
-
+def startup():
+    if sys.platform != "linux":
+        messagebox.showerror("Error", "Sorry but this script only works on Linux!")
+        sys.exit()
+    if sys.version_info.major < 3:
+        messagebox.showerror("Error", "The script requires python 3 or above!")
+        sys.exit()
+    if (popen("systemctl is-active  warp-svc").read()).rstrip() == "active":
+        messagebox.showerror("Error", "Start daemon from CLI with\n'sudo systemctl start warp-svc'\nand ensure registration has run first.")
+        sys.exit()
+        
 def enableCallback():
     popen("warp-cli connect").read()
     statusCheck()
@@ -42,8 +54,15 @@ def statusCheck():
         root.iconphoto(True, orange)
     return True
 
+def on_exit():
+    popen("warp-cli disconnect").read()
+    root.quit()
+    
+
 Button(root, text="Enable", command=lambda: Thread(target=enableCallback).start(), width=10).grid(row=1, column=2)
 Button(root, text="Disable", command=lambda: Thread(target=disableCallback).start(), width=10).grid(row=2, column=2)
 
+root.protocol("WM_DELETE_WINDOW", on_exit)
+startup()
 statusCheck()
 root.mainloop()
