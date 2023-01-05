42763abf3fc2c73e15c2677e294f127ffc6d943b
DNAMcKnight <conner.cmk9040@gmail.com>
2023-01-06 00:08:49 +0600

    added a settings file and a startup message for first time users

--- old_app.py
+++ old_app.py
@@ -1,9 +1,9 @@
-from tkinter import Tk, Label, Button,messagebox
-from tkinter import StringVar, PhotoImage
+from tkinter import Tk, Label, Button,messagebox, Checkbutton
+from tkinter import StringVar, PhotoImage, IntVar
 from threading import Thread
 from os import popen, getcwd
 from time import sleep
-import sys
+import sys, json
 
 root = Tk()
 root.title("Cloudflare WARP")
@@ -17,17 +17,29 @@ enableLabel = Label(root, text="Enable Cloudflare WARP", width=50).grid(row=1, c
 disableLabel = Label(root, text="Disable Cloudflare WARP", width=50).grid(row=2, column=1)
 statusLabel = Label(root, textvariable=status, width=50).grid(row=3, column=1, columnspan=2, pady=110)
 
+def introMessage():
+    with open("settings.json", "r") as f:
+        settings = json.load(f)
+        if settings["startupMsg"]:
+            messagebox.showinfo("Thank You!", "Thank you for using my App, please support me by giving a star ⭐ !")
+        with open("settings.json", "w") as write:
+            settings['startupMsg'] = False
+            json.dump(settings, write)
+
 def startup():
+    introMessage()
     if sys.platform != "linux":
         messagebox.showerror("Error", "Sorry but this script only works on Linux!")
         sys.exit()
     if sys.version_info.major < 3:
         messagebox.showerror("Error", "The script requires python 3 or above!")
         sys.exit()
-    if (popen("systemctl is-active  warp-svc").read()).rstrip() == "active":
+    if (popen("systemctl is-active  warp-svc").read()).rstrip() == "inactive":
         messagebox.showerror("Error", "Start daemon from CLI with\n'sudo systemctl start warp-svc'\nand ensure registration has run first.")
         sys.exit()
-        
+
+
+
 def enableCallback():
     popen("warp-cli connect").read()
     statusCheck()
