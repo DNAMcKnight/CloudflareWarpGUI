a9ef569582054513382a58b837c609a9bf73a3ba
DNAMcKnight <conner.cmk9040@gmail.com>
2023-01-06 14:23:34 +0600

    customTkinter doesn't have a message box in which case I choose to update the GUI for erros

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from customtkinter import StringVar
 from threading import Thread
 from os import popen
 from time import sleep
+import sys
 
 tk.set_appearance_mode("dark")
 tk.set_default_color_theme("dark-blue")
@@ -12,12 +13,38 @@ class App(tk.CTk):
         self.status = StringVar()
         self.title("Cloudflare WARP")
         self.geometry("470x200")
+        self.dependencyCheck()
+        self.statusCheck()
+        
+    def dependencyCheck(self):
+        check = True
+        # check if user is using linux
+        if sys.platform != "linux":
+            tk.CTkLabel(master=self, text="Sorry but this script only works on Linux!", width=375).pack(pady=50)
+            tk.CTkButton(master=self, text="Exit", command=lambda: Thread(target=sys.exit(), width=75)).pack(pady=10)
+            check = False
+        # check the python version
+        if sys.version_info.major < 3:
+            tk.CTkLabel(master=self, text="The script requires python 3 or above!", width=375).pack(pady=50)
+            tk.CTkButton(master=self, text="Exit", command=lambda: Thread(target=sys.exit(), width=75)).pack(pady=10)
+            check = False
+        # check if the daemon is running
+        if (popen("systemctl is-active  warp-svc").read()).rstrip() == "inactive":
+            tk.CTkLabel(master=self, text="Start daemon from CLI with\n'sudo systemctl start warp-svc'\nand ensure registration has run first.", width=375).pack(pady=20)
+            tk.CTkButton(master=self, text="Exit", command=lambda: Thread(target=sys.exit(), width=75)).pack(pady=10)
+            check = False
+        self.menu() if check else ""
+        return True
+        
+    # generates the menu to enable/disable warp
+    def menu(self):
         tk.CTkLabel(master=self, text="Enable Cloudflare WARP", width=375).grid(row=1, column=1, pady=10)
         tk.CTkLabel(master=self, text="Disable Cloudflare WARP", width=375).grid(row=2, column=1)
         tk.CTkLabel(master=self, textvariable=self.status, width=50,font=("Arial", 10)).grid(row=3, column=1,columnspan=2, pady=100)
         tk.CTkButton(master=self, text="Enable", command=lambda: Thread(target=self.enableCallback).start(), width=75).grid(row=1, column=2)
         tk.CTkButton(master=self, text="Disable", command=lambda: Thread(target=self.disableCallback).start(), width=75, ).grid(row=2, column=2, padx=5)
-        self.statusCheck()
+    
+    # connect callback, it runs status check to update until connection status changes to connected
     def enableCallback(self):
         popen("warp-cli connect").read()
         self.statusCheck()
