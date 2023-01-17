68dc775b6ab0dfac2dc042d1f0f0f0642154458d
DNAMcKnight <conner.cmk9040@gmail.com>
2023-01-17 23:53:30 +0600

    added check button for the toolbar icon

--- old_app.py
+++ old_app.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Label, Button,messagebox, Checkbutton
+from tkinter import Tk, Label, Button,messagebox, Checkbutton,Canvas
 from tkinter import StringVar, PhotoImage, IntVar
 from threading import Thread
 from os import popen, getcwd
@@ -8,15 +8,21 @@ import sys, json
 root = Tk()
 root.title("Cloudflare WARP")
 root.geometry("470x200")
-root.resizable(False, False)
+# root.configure(bg="#000000")
+# root.resizable(False, False)
 white = PhotoImage(file=f"{getcwd()}/images/white.png")
 orange = PhotoImage(file=f"{getcwd()}/images/orange.png")
 root.iconphoto(True, white)
 status = StringVar()
-enableLabel = Label(root, text="Enable Cloudflare WARP", width=50).grid(row=1, column=1, pady= 10)
-disableLabel = Label(root, text="Disable Cloudflare WARP", width=50).grid(row=2, column=1)
-statusLabel = Label(root, textvariable=status, width=50).grid(row=3, column=1, columnspan=2, pady=110)
+enableLabel = Label(root, text="Enable Cloudflare WARP").grid(row=0, column=0, padx=90, pady=5)
+Button(root, text="Enable", command=lambda: Thread(target=enableCallback).start(), width=10).grid(row=0, column=1,padx=5, pady=5)
 
+disableLabel = Label(root, text="Disable Cloudflare WARP").grid(row=1, column=0,padx=5, pady=5)
+Button(root, text="Disable", command=lambda: Thread(target=disableCallback).start(), width=10).grid(row=1, column=1,padx=5, pady=5)
+
+checkButton = Checkbutton(root, text="Enable WARP toolbar icon").grid(row=2,columnspan=2, pady=5)
+
+statusLabel = Label(root, textvariable=status, width=50).grid(row=3,columnspan=2, pady=60)
 def introMessage():
     with open("settings.json", "r") as f:
         settings = json.load(f)
@@ -42,6 +48,7 @@ def startup():
 
 def enableCallback():
     popen("warp-cli connect").read()
+    root.geometry("470x200")
     statusCheck()
     root.iconphoto(True, orange)
     return True
@@ -71,8 +78,7 @@ def on_exit():
     root.quit()
     
 
-Button(root, text="Enable", command=lambda: Thread(target=enableCallback).start(), width=10).grid(row=1, column=2)
-Button(root, text="Disable", command=lambda: Thread(target=disableCallback).start(), width=10).grid(row=2, column=2)
+
 
 root.protocol("WM_DELETE_WINDOW", on_exit)
 startup()
