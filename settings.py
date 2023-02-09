ad99562a2f143b8d77171b3fa753f2ae3612eec2
DNAMcKnight <conner.cmk9040@gmail.com>
2023-02-10 01:01:11 +0600

    updated the default settings added change and check function

--- settings.py
+++ settings.py
@@ -1,12 +1,38 @@
 from pathlib import Path
-import json
+import json, sys
+
+if getattr(sys, 'frozen', False):
+    path = f"{sys._MEIPASS}/"
+else:
+    path = ""
 
 def startup():
-    default = {"startupMsg": True}
-    settings = Path('config.json')
+    default = {
+    "startupMsg": True,
+    "winWarningMsg": True,
+    "defaultTaskbar": False,
+    "autoConnect": False,
+    "keepAlive": False
+}
+    settings = Path(f'{path}config.json')
     if settings.exists():
         return True
     with open('config.json', 'w') as f:
-        json.dump(default, f)
+        json.dump(default, f, indent=2)
     return False
 
+def change(key, value):
+    with open(f"{path}config.json", "r") as f:
+        data = json.load(f)
+        with open(f"{path}config.json", "w") as write:
+            data[key] = value
+            json.dump(data, write, indent=2)
+            return True
+    return False
+
+def check(key):
+    with open(f"{path}config.json", "r") as f:
+        data = json.load(f)
+        if key in data:
+            return data[key]
+        return None
